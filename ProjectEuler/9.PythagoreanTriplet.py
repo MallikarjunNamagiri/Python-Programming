a335ef2188f098c3e6f1af2dc02d246a55654673
Mallikarjun Namagiri <30703513+MallikarjunNamagiri@users.noreply.github.com>
2019-12-22 12:37:04 +0530

    Add files via upload

---
+++ ProjectEuler/9.PythagoreanTriplet.py
@@ -0,0 +1,14 @@
+def euler9():
+    # Ensure a < c
+    for c in range(2, 1000):
+        for a in range(1, c):
+            # Ensure a + b + c == 1000.  Since a is counting up, the first
+            # answer we find should have a <= b.
+            b = 1000 - c - a
+
+            # Ensure Pythagorean triple
+            if a**2 + b**2 == c**2:
+                print("a = %d, b = %d, c = %d.  abc = %d" % (a, b, c, a * b * c))
+                return
+
+euler9()
