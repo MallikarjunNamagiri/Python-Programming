bf7425057ac22dc6732c20a2f7305b6121708c0b
MallikarjunNamagiri <mallik.namagiri@gmail.com>
2020-04-28 23:43:15 +0530

    Unit Testing

---
+++ Basics/Python_17 - Unit Testing/test_calc.py
@@ -0,0 +1,16 @@
+# https://docs.python.org/3
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        result = calc.add(10, 5)
+        self.assertEqual(result, 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+#python -m unittest test_calc.py
