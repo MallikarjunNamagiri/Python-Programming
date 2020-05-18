55f09ad1262c3afe45b2ae8f83338d922eb543ff
MallikarjunNamagiri <mallik.namagiri@gmail.com>
2020-05-18 23:36:38 +0530

    flask post updated

--- Flask Tutorial/FlaskTutorial/routes.py
+++ Flask Tutorial/FlaskTutorial/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from FlaskTutorial.models import User, Post
 from FlaskTutorial.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from FlaskTutorial import app, db, bcrypt
@@ -97,9 +97,26 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.data, author=current_user)
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update")
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    return render_template('create_post.html', title='Update Post', form=form)
+
